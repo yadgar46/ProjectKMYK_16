b8965e74a65fa78a755e0e3edb8758b981215c20
yadgar46 <59146979+yadgar46@users.noreply.github.com>
2020-05-14 19:20:52 +0500

    version №2
    Добавление действий на кнопки вход

--- Курсовая/src/Start.java
+++ Курсовая/src/Start.java
@@ -1,4 +1,4 @@
-
+package dfs;
 
 //импорт библиотек
 import java.awt.event.MouseAdapter;
@@ -25,7 +25,7 @@ setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //действие при наж
  JTextField tf3 = new JTextField(17);
  JButton btEnter = new JButton("Вход");
  JLabel timer = new JLabel();
- //дабавление элементов 
+ //дабовление элементов 
  panel0.add(new JLabel("Введите логин"));
  panel0.add(tf3);
  panel0.add(new JLabel("Введите пароль"));
@@ -33,11 +33,42 @@ setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //действие при наж
  panel0.add(btEnter);
  panel0.add(timer);
  
- setContentPane(panel0);//настройка отображения 
+ setContentPane(panel0);//настройка отображения
  setSize(350, 150);
  setVisible(true);
  
  
+ //кнопка вход
+ btEnter.addMouseListener(new MouseAdapter(){
+	 public void mouseClicked(MouseEvent event) {
+	if(tf3.getText().equals("login")) {// логин 
+		 if (tf2.getText().equals("123")) { //пароль
+			
+			Calculator forms =new Calculator();//открытие калькулятора
+			forms.setVisible(true); 
+			setVisible(false);
+		 }
+		 else {//ход не выполнен
+			 JOptionPane.showMessageDialog(null, "Вход НЕ выполнен");
+			 count++;
+			 if (count == 2) {
+				 JOptionPane.showMessageDialog(null, "Пожалуйста проверьте данные ");//вход не выполнен второй раз
+				 System.exit(EXIT_ON_CLOSE);
+				 
+			 }
+		 }
+ }else {//ход не выполнен
+	 JOptionPane.showMessageDialog(null, "Вход НЕ выполнен");
+	 count++;
+	 if (count == 2) {
+		 JOptionPane.showMessageDialog(null, "Пожалуйста проверьте данные ");//вход не выполнен второй раз
+		 System.exit(EXIT_ON_CLOSE);
+		 
+	 }
+ }
+ }});
+ 
+
  }
 public static void main(String[] args)
 {
